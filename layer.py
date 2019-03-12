cc6780fdbcccea79be3051c4c3757de4a30db08a
MichalTurski <turski.michal.antoni@gmail.com>
2019-03-12 10:07:07 +0100

    Checking numerical gradient for every element of weights matrix.

--- layer.py
+++ layer.py
@@ -75,18 +75,29 @@ class Layer:
 
     def check_gradient(self, a_in, t):
         prev_gw = copy.copy(self.gw)  # Copying it is an ugly hack, but it's only debug module
+        prev_gb = copy.copy(self.gb)
+        numerical_grad = np.empty_like(self.w)
+        # Calculate gradient with backpropagation
         self.teach(a_in, t)
         curr_gw = self.gw - prev_gw
+        # Calculate numerical gradient
         eps = 0.05
         eps_arr = np.full_like(self.gw, 0)
-        eps_arr[0, 0] = eps
-        self.w += eps_arr
-        up_cost = self.cost_fun(a_in, t)
-        self.w -= 2 * eps_arr
-        down_cost = self.cost_fun(a_in, t)
-        self.w += eps_arr
-        numerical_grad = (up_cost - down_cost) / (2 * eps)
+        for i in range(self.w.shape[0]):
+            for j in range(self.w.shape[1]):
+                eps_arr[i, j] = eps
+                self.w += eps_arr
+                up_cost = self.cost_fun(a_in, t)
+                self.w -= 2 * eps_arr
+                down_cost = self.cost_fun(a_in, t)
+                self.w += eps_arr
+                numerical_grad[i, j] = (up_cost - down_cost) / (2 * eps)
+                eps_arr[i, j] = 0
         print(f"numerical grad = {numerical_grad}, backpropagated = {curr_gw[0, 0]}")
+        # Undo changes done in teach
+        self.gw = prev_gw
+        self.gb = prev_gb
+
 
 
 class OutLayer:
